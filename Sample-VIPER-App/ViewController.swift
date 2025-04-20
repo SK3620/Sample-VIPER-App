e1499acc81c76d5da06fe818e58cdf2ce05ff0f1
SK3620 <k.n.t11193620@gmail.com>
2025-04-20 20:04:46 +0900

    Initial Commit

---
+++ Sample-VIPER-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sample-VIPER-App
+//
+//  Created by 鈴木 健太 on 2025/04/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
