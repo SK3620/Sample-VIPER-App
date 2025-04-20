0aa8d720f7ffa4d53fde9d8f1ac7347abdfa1b8c
SK3620 <k.n.t11193620@gmail.com>
2025-04-20 20:17:40 +0900

    add: 記事リスト画面を作成

---
+++ Sample-VIPER-App/ArticleListViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Sample-VIPER-App
+//
+//  Created by 鈴木 健太 on 2025/04/20.
+//
+
+import UIKit
+
+class ArticleListViewController: UIViewController {
+    
+    private var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+}
+
+extension ArticleListViewController: UITableViewDelegate, UITableViewDataSource {
+     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "記事のタイトル"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
