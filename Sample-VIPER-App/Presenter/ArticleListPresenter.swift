0707c17aba514fce332a0c283b31d7c612c76412
SK3620 <k.n.t11193620@gmail.com>
2025-04-21 21:46:58 +0900

    add: Presenterを追加

---
+++ Sample-VIPER-App/Presenter/ArticleListPresenter.swift
@@ -0,0 +1,64 @@
+//
+//  ArticleListPresenter.swift
+//  Sample-VIPER-App
+//
+//  Created by 鈴木 健太 on 2025/04/21.
+//
+
+import Foundation
+
+protocol ArticleListPresenterProtocol: AnyObject {
+    
+    func didLoad()
+    func didSelect(articleEntity: ArticleEntity)
+}
+
+protocol ArticleListViewProtocol: AnyObject {
+    
+    func showArticles(_ articleEntities: [ArticleEntity])
+    func showEmpty()
+    func showError(_ error: Error)
+}
+
+class ArticleListPresenter {
+    
+    struct Dependency {
+        let router: ArticleListRouterProtocol!
+        let getArticlesArrayUseCase: UseCase<Void, [ArticleEntity], Error>
+    }
+    
+    weak var view: ArticleListViewProtocol!
+    private var di: Dependency
+    
+    init(view: ArticleListViewProtocol, inject dependency: Dependency) {
+        self.view = view
+        self.di = dependency
+    }
+}
+
+extension ArticleListPresenter: ArticleListPresenterProtocol {
+        
+    func didLoad() {
+        // GetArticlesArrayUseCase().execute(()) { [weak self] result in
+        di.getArticlesArrayUseCase.execute(()) { [weak self] result in
+
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let articleEntities):
+                if articleEntities.isEmpty {
+                    self.view.showEmpty()
+                    return
+                }
+                self.view.showArticles(articleEntities)
+            case .failure(let error):
+                self.view.showError(error)
+            }
+        }
+    }
+    
+    func didSelect(articleEntity: ArticleEntity) {
+        di.router.showArticleDetail(articleEntity: articleEntity)
+    }
+}
+
