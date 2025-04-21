5a3effb4188ad58644e0ab59f0e26d56d2031eb2
SK3620 <k.n.t11193620@gmail.com>
2025-04-21 21:45:44 +0900

    add: Entityを追加

---
+++ Sample-VIPER-App/Entity/ArticleEntity.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleEntity.swift
+//  Sample-VIPER-App
+//
+//  Created by 鈴木 健太 on 2025/04/20.
+//
+
+import Foundation
+
+struct ArticleEntity: Codable {
+    let id: Int
+    let userId: Int
+    let title: String
+    let body: String
+}
