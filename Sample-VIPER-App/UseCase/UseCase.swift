f6c885bd380c7a7388c222bca6b73beefd2adc32
SK3620 <k.n.t11193620@gmail.com>
2025-04-21 21:45:22 +0900

    add: UseCaseを追加

---
+++ Sample-VIPER-App/UseCase/UseCase.swift
@@ -0,0 +1,57 @@
+//
+//  UsecaseProtocol.swift
+//  Sample-VIPER-App
+//
+//  Created by 鈴木 健太 on 2025/04/21.
+//
+
+import Foundation
+
+// ユースケースの共通化
+protocol UseCaseProtocol where Failure: Error {
+    associatedtype Parameter
+    associatedtype Success
+    associatedtype Failure
+    
+    func execute(_ parameter: Parameter, completion: ((Result<Success, Failure>) -> ())?)
+}
+
+class UseCase<Parameter, Success, Failure: Error> {
+    
+    private let instance: UseCaseInstanceBase<Parameter, Success, Failure>
+    
+    init<T: UseCaseProtocol>(_ useCase: T) where
+        T.Parameter == Parameter,
+        T.Success == Success,
+        T.Failure == Failure
+    {
+        self.instance = UseCaseInstance<T>(useCase)
+    }
+    
+    func execute(_ parameter: Parameter, completion: ((Result<Success, Failure>) -> ())?) {
+        instance.execute(parameter, completion: completion)
+    }
+}
+
+private extension UseCase {
+    
+    class UseCaseInstanceBase<Parameter, Success, Failure: Error> {
+        
+        func execute(_ parameter: Parameter, completion: ((Result<Success, Failure>) -> ())?) {
+            fatalError()
+        }
+    }
+    
+    class UseCaseInstance<T: UseCaseProtocol>: UseCaseInstanceBase<T.Parameter, T.Success, T.Failure> {
+        
+        private let useCase: T
+        
+        init(_ useCase: T) {
+            self.useCase = useCase
+        }
+        
+        override func execute(_ parameter: T.Parameter, completion: ((Result<T.Success, T.Failure>) -> ())?) {
+            useCase.execute(parameter, completion: completion)
+        }
+    }
+}
