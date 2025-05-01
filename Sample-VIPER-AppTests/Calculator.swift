39233bbb71e68c554b3e75ca10e6beec24600acb
SK3620 <k.n.t11193620@gmail.com>
2025-05-02 08:38:08 +0900

    add: 練習用テストコードを追加

---
+++ Sample-VIPER-AppTests/Calculator.swift
@@ -0,0 +1,29 @@
+//
+//  Utils.swift
+//  Sample-VIPER-App
+//
+//  Created by 鈴木 健太 on 2025/05/01.
+//
+
+
+import Foundation
+
+// 練習
+struct Calculator {
+    // テスト用足し算関数
+    public func calcAdd(a: Int , b: Int)  -> Int {
+        return a + b
+    }
+    // テスト引き算関数
+    public func calcSub(a: Int , b: Int)  -> Int {
+        return a - b
+    }
+    // テスト用割り算関数
+    public func calcDiv(a: Int , b: Int)  -> Int {
+        return a / b
+    }
+    // テスト用掛け算関数
+    public func calcMulti(a: Int , b: Int)  -> Int {
+        return a * b
+    }
+}
--- Sample-VIPER-AppTests/Sample_VIPER_AppTests.swift
+++ Sample-VIPER-AppTests/Sample_VIPER_AppTests.swift
@@ -6,9 +6,35 @@
 //
 
 import XCTest
+
+// @testable import CRUD（アプリ名）と記載することで（CRUD）アプリ内のpublic,internalシンボルのテストが可能になります。
+// 今回のアプリ名は「Sample_VIPER_App」
 @testable import Sample_VIPER_App
 
 final class Sample_VIPER_AppTests: XCTestCase {
+    
+    /*
+     setUpWithError()
+     →各テストを実行する前に毎回呼ばれます。
+     
+     tearDownWithError()`
+     →各テスト実行後に毎回呼ばれます。
+     
+     testExample()
+     →テストコードのサンプルです。<test + テスト対象のメソッド名>
+     
+     testPerformanceExample()
+     →パフォーマンステストのサンプルです。<testPerformance + テスト対象のメソッド名>
+     
+     
+     実行順番
+     setUpWithError()
+     testExample()
+     tearDownWithError()
+     setUpWithError()
+     testCulcAdd() （test任意のメソッド名()）
+     tearDownWithError()
+     */
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -32,5 +58,29 @@ final class Sample_VIPER_AppTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+    
+    // MARK: - 練習
+    // 足し算
+    func testCulcAdd() throws {
+        XCTAssertEqual(Calculator().calcAdd(a: 27 , b: 2) , 29)
+        XCTAssertEqual(Calculator().calcAdd(a: 2 , b: 2) , 4)
+    }
 
+    // 引き算
+    func testCulcSub() throws {
+        XCTAssertEqual(Calculator().calcSub(a: 27 , b: 2) , 25)
+        XCTAssertEqual(Calculator().calcSub(a: 2 , b: 22) , -20)
+    }
+
+    // 割り算
+    func testCulcDiv() throws {
+        XCTAssertEqual(Calculator().calcDiv(a: 27 , b: 2) , 13)
+        XCTAssertEqual(Calculator().calcDiv(a: 2 , b: 2) , 1)
+    }
+
+    // 掛け算
+    func testCulcMulti()throws {
+        XCTAssertEqual(Calculator().calcMulti(a: 27 , b: 2) , 54)
+        XCTAssertEqual(Calculator().calcMulti(a: 3 , b: 2) , 6)
+    }
 }
